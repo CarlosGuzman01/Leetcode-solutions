6083c1b5643213b153ec832c72cef3e7edd605ef
CarlosGuzman01 <carlosguzmantejeda13@gmail.com>
2023-10-03 14:58:47 -0400

    solved valid anagram

---
+++ Algorithms/Java/242-valid_anagram.java
@@ -0,0 +1,19 @@
+//version 1
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] charArray1 = s.toCharArray();
+        char[] charArray2 = t.toCharArray();
+
+       Arrays.sort(charArray1);
+       Arrays.sort(charArray2);
+
+
+       if(Arrays.equals(charArray1, charArray2)){
+        return true;
+       }
+
+       return false;
+              
+    }
+}
