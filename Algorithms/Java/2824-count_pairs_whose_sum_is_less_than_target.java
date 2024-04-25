b55632a766e552728f56cdd7b2d95217472aaaa9
CarlosGuzman01 <carlosguzmantejeda13@gmail.com>
2024-04-25 05:11:50 -0400

    done 2824

---
+++ Algorithms/Java/2824-count_pairs_whose_sum_is_less_than_target.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countPairs(List<Integer> nums, int target) {
+
+        int counter = 0;
+
+        Collections.sort(nums);
+
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = i + 1; j < nums.size();  j++){
+               
+                if(nums.get(i) + nums.get(j) >= target) break;
+                counter++;
+            }
+        }
+
+        return counter;    
+    }
+}
