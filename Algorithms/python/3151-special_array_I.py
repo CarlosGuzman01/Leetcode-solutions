05802423fd3a6011b9566ecb058a43aaac5609b4
CarlosGuzman01 <120758068+CarlosGuzman01@users.noreply.github.com>
2024-07-03 04:59:04 -0400

    improved solution 3151

--- Algorithms/python/3151-special_array_I.py
+++ Algorithms/python/3151-special_array_I.py
@@ -3,9 +3,7 @@ class Solution:
         
         i = 0
         while i + 1 < len(nums):
-            if nums[i] % 2 == 0 and nums[i + 1] % 2 == 0:
-                return False
-            elif nums[i] % 2 != 0 and nums[i + 1] % 2 != 0:
+            if (nums[i] % 2 == 0) == (nums[i + 1] % 2 == 0):
                 return False
             i += 1
         return True
