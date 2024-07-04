0f765799c672a5c37b273c76e416a0bae1502214
CarlosGuzman01 <120758068+CarlosGuzman01@users.noreply.github.com>
2024-07-04 14:09:26 -0400

    done 2496

---
+++ Algorithms/python/2496-maximum_value_of_a_string_in_an_array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maximumValue(self, strs: List[str]) -> int:
+        res = 0
+
+        for s in strs:
+            if s.isnumeric():
+                res = max(res, int(s))
+            else:
+                res = max(res, len(s))
+        return res
