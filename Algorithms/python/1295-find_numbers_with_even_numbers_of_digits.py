54a6f84d5f9d0d9c6c9e54855e506fc5aab54d10
CarlosGuzman01 <120758068+CarlosGuzman01@users.noreply.github.com>
2024-08-03 17:28:18 -0400

    done 1295

---
+++ Algorithms/python/1295-find_numbers_with_even_numbers_of_digits.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        counter = 0
+        for n in nums:
+            if len(str(n)) % 2 == 0:
+                counter += 1
+        return counter
+
+
+   
