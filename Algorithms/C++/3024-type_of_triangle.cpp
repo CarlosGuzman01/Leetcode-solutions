51329177f4ed975260bffa0d0d830fe2b38b400b
CarlosGuzman01 <120758068+CarlosGuzman01@users.noreply.github.com>
2024-10-26 20:36:03 -0400

    done 3024

---
+++ Algorithms/C++/3024-type_of_triangle.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string triangleType(vector<int>& nums) {
+
+        int a = nums[0];
+        int b = nums[1];
+        int c = nums[2];
+
+        if(a + b <= c || c + b <= a || a + c <= b){
+            return "none";
+        }
+
+        if(a == b && b == c){
+            return "equilateral";
+        }
+
+        if(a == b || a == c || b == c){
+            return "isosceles";
+        }
+
+       return "scalene";
+        
+    }
+};
