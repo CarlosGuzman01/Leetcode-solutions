ddf312877ad2ead048598268787d47a7a8b89f89
Your Name <carlosguzmantejeda13@gmail.com>
2025-03-03 03:29:57 -0500

    done 2161

---
+++ Algorithms/python/2161-partition_array_according_to_given_pivot.py
@@ -0,0 +1,34 @@
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        
+        res = [0] * len(nums)
+
+        i = 0
+        j = len(nums) - 1
+
+        i2 = 0
+        j2 = len(nums) - 1
+
+        while i < len(nums):
+
+            if nums[i] < pivot:
+                res[i2] = nums[i]
+                i2 += 1
+            if nums[j] > pivot:
+                res[j2] = nums[j]
+                j2 -= 1
+            
+            i += 1
+            j -= 1
+        
+        while i2 <= j2:
+            res[i2] = pivot
+            res[j2] = pivot
+
+            i2 += 1
+            j2 -= 1
+
+        return res
+
+
+        
